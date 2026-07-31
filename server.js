50d30bfeea3413874e95d02d23da4559a32d4138
Sanaul islam <57231477+sanaulislam223@users.noreply.github.com>
2026-07-31 19:25:47 +0530

    Add CORS support and update log messages

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const path = require('path');
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    console.log('User connected');
+    console.log('Ek user connect hua:', socket.id);
 
-    socket.on('message', (data) => {
-        socket.broadcast.emit('message', data);
+    // Jab koi message bheje, toh server use SABHI ko deliver karega
+    socket.on('chat-message', (data) => {
+        io.emit('chat-message', data); 
     });
 
-    socket.on('signal', (data) => {
-        socket.broadcast.emit('signal', data);
+    socket.on('webrtc-signal', (data) => {
+        socket.broadcast.emit('webrtc-signal', data);
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        console.log('User disconnect hua:', socket.id);
     });
 });
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
